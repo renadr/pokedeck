72c4c9087f138df6049a5411b41cd23c105265b0
Adrien Lemaire <ladrien2@gmail.com>
2017-11-01 19:53:08 +0100

    Creating the deck class with its constructor

---
+++ pokedeck/src/main/java/upmc/pcg/Deck.java
@@ -0,0 +1,15 @@
+package upmc.pcg;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Adrien on 01/11/2017.
+ */
+public class Deck {
+
+    private ArrayList<Card> deck;
+
+    public Deck(ArrayList<Card> deck) {
+        this.deck = deck;
+    }
+}
--- pokedeck/src/main/java/upmc/pcg/Pokedeck.java
+++ pokedeck/src/main/java/upmc/pcg/Pokedeck.java
@@ -16,8 +16,7 @@ package upmc.pcg;
 
 import upmc.pcg.ui.GameUI;
 
-public class Pokedeck
-{
+public class Pokedeck {
   public static void main(String[] args) {
     GameUI game_ui = new GameUI();
     game_ui.start();
