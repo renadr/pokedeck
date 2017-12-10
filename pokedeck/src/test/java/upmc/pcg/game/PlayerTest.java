b67d56404c94a70506381af9a3057a669650544e
Adrien Lemaire <ladrien2@gmail.com>
2017-12-10 16:38:22 +0100

    Junit methods test for Player

--- pokedeck/src/test/java/upmc/pcg/game/PlayerTest.java
+++ pokedeck/src/test/java/upmc/pcg/game/PlayerTest.java
@@ -2,6 +2,9 @@ package upmc.pcg.game;
 
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import static org.junit.Assert.*;
 
 /**
@@ -17,22 +20,33 @@ public class PlayerTest {
 
     @Test
     public void test_get_deck() throws Exception {
+//        String input = "1";
+//        InputStream in = new ByteArrayInputStream(input.getBytes());
+//        System.setIn(in);
+//        this.p1.add_card("ENERGY");
+//        System.setIn(System.in);
+//        assertEquals(this.p1.get_deck(),this.p1.get_deck());
+//        this.p1.add_card("POKEMON");
+//        this.p1.add_card("TRAINER");
+//        this.p1.add_card("DIGIMON");
 
     }
 
     @Test
     public void test_get_password() throws Exception {
-
+        String pwd = "hello";
+        this.p1.set_password(pwd);
+        assertEquals(this.p1.get_password(),pwd);
     }
 
     @Test
     public void test_save_deck() throws Exception {
-
+        assertEquals(this.p1.save_deck(),"Save is done !");
     }
 
     @Test
     public void test_load_deck() throws Exception {
-
+        assertEquals(this.p1.load_deck(),"Success !");
     }
 
 }
